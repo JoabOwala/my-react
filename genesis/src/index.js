e3e31cd8ad3fe0fe9c8b904b18c4d542d328c482
Joab Owala <owalajoab@gmail.com>
2023-04-25 12:16:25 +0300

    updated with props

--- genesis/src/index.js
+++ genesis/src/index.js
@@ -4,34 +4,24 @@ import './index.css'
 
 function BookList(){
   return (
-    <section className='booklist'>
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-    </section>
+  <section className='booklist'>
+    <Book />
+  </section>
   )
 }
 
-const Book = () => {
-  return <article className='book'>
-    <Image />
-    <Title />
-    <Author />
-  </article>
+const Book = () =>{
+  const title = 'Greenligts'
+  const author = 'Matthew McConaughey'
+  return (
+    <article className='book'>
+      <img src="https://m.media-amazon.com/images/I/41xNw77nsCL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg" 
+        alt="" />
+      <h1>{title}</h1>
+      <h4>{author.toUpperCase()}</h4>
+    </article>
+  )
 }
-
-const Image = () => {
-  return <img src="https://m.media-amazon.com/images/I/41xNw77nsCL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg" 
-  alt="" />
-}
-
-const Title = () => <h1>Greenligts</h1>
-
-
-const Author = () => <h4 style={{color:'#617d98', fontSize: '0.75rem', margin:'0.25rem'}}>Matthew McConaughey</h4>
-
 
 ReactDOM.render(
   <BookList />,
