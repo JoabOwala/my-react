fa4b4220340bb1da52717a52a552e0cd14586bdc
Joab Owala <owalajoab@gmail.com>
2023-04-25 10:43:36 +0300

    removing deleted files from tracking

--- genesis/src/index.js
+++ genesis/src/index.js
@@ -1,17 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function BookList(){
+  return (
+    <section className='booklist'>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Book = () => {
+  return <article className='book'>
+    <Image />
+    <Title />
+    <Author />
+  </article>
+}
+
+const Image = () => {
+  return <img src="https://m.media-amazon.com/images/I/41xNw77nsCL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg" 
+  alt="" />
+}
+
+const Title = () => <h1>Greenligts</h1>
+
+
+const Author = () => <h4 style={{color:'#617d98', fontSize: '0.75rem', margin:'0.25rem'}}>Matthew McConaughey</h4>
+
+
+ReactDOM.render(
+  <BookList />,
+  document.getElementById('root')
+)
