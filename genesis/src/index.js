74be42b52af37c13099e4a4df0728aec98b7383c
Joab Owala <owalajoab@gmail.com>
2023-04-25 12:17:06 +0300

    working with props

--- genesis/src/index.js
+++ genesis/src/index.js
@@ -2,23 +2,44 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 
+//setup vars
+
+const firstBook = {
+  img: 'https://m.media-amazon.com/images/I/41xNw77nsCL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg',
+  title: 'Greenligts',
+  author: 'Matthew McConaughey'
+}
+
+const secondBook = {
+  img: 'https://m.media-amazon.com/images/I/51eDMGwZVvL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg',
+  title: 'I Will Teach You to Be Rich',
+  author: 'Ramit Sethi'
+}
+
 function BookList(){
   return (
   <section className='booklist'>
-    <Book />
+    <Book 
+    img={firstBook.img}
+    title={firstBook.title}
+    author={firstBook.author}
+    
+    />
+    <Book 
+    img={secondBook.img}
+    title={secondBook.title}
+    author={secondBook.author}
+    />
   </section>
   )
 }
 
-const Book = () =>{
-  const title = 'Greenligts'
-  const author = 'Matthew McConaughey'
+const Book = (props) =>{
   return (
     <article className='book'>
-      <img src="https://m.media-amazon.com/images/I/41xNw77nsCL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg" 
-        alt="" />
-      <h1>{title}</h1>
-      <h4>{author.toUpperCase()}</h4>
+      <img src={props.img} alt=''/>
+      <h1>{props.title}</h1>
+      <h4>{props.author}</h4>
     </article>
   )
 }
